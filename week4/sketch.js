0e68634b1a22ff2bd13231bd4e219f351ed11db6
Jenny Chen <jenjennychen@gmail.com>
2017-09-26 19:32:56 -0400

    initial commit

--- week4/sketch.js
+++ week4/sketch.js
@@ -7,16 +7,55 @@ function keyPressed() {
 	background(640, 480);
 }
 var circleX=0;
+var text1 = "Draw";
+var text2 = "some"
+var text3 = "crazy"
+var text4 = "hair"
+
 
 function draw() {
-    strokeWeight(2);
-	stroke(84,153,199);
-	fill(93,109,126);
     
+   //text// 
+    textSize(30);
+	fill("Deeppink");
+	noStroke();
+	textFont("Comic Sans MS");
+	text(text1, 5, 10, 20, 200);
     
-//face//
-    fill(245,251,112);
-	ellipse(width/2, height/2, width - mouseX);
+    textSize(30);
+	fill("Deeppink");
+	noStroke();
+	textFont("Comic Sans MS");
+	text(text2, 90, 10, 20, 200);
+    
+    textSize(30);
+	fill("Deeppink");
+	noStroke();
+	textFont("Comic Sans MS");
+	text(text3, 174, 10, 20, 200);
+    
+    textSize(30);
+	fill("Deeppink");
+	noStroke();
+	textFont("Comic Sans MS");
+	text(text4, 260, 10, 20, 200);
+
+//mouse//
+    strokeWeight(6);
+	stroke("red");
+	fill("red");
+	line(mouseX, mouseY, pmouseX, pmouseY)
+//face
+    //strokeWeight(2);
+	//stroke(84,153,199);
+	//fill(93,109,126);
+    //fill(245,251,112);
+	//ellipse(width/2, height/2, width - mouseX);
+    
+//face2
+    stroke("MidnightBlue");
+	fill("yellow");
+	ellipse(width/2, height/2, frameCount, frameCount);
     
 //eyes//
     stroke(0,0,0);
@@ -30,8 +69,13 @@ function draw() {
     ellipse(195,170,45,80);
     ellipse(410,170,45,80);
 
-//moving ellipse//
+//mouth
+    noStroke();
+    fill("red");
+    ellipse(300,350,200,50);
+
+//moving ellipse
     fill(241,243,200);
-    ellipse(circleX,300,100,100);
-    circleX = circleX + 1;      
+    ellipse(circleX,150,20,20);
+    circleX = circleX + .5;      
 }
