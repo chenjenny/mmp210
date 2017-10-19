0826da1b84b5d09216d6d642024e0dbe174bbdc6
Jenny Chen <jenjennychen@gmail.com>
2017-10-19 16:40:48 -0400

    initial commit

--- week7/sketch.js
+++ week7/sketch.js
@@ -5,26 +5,62 @@ function setup() {
     
     //brick building
     var space = 50;
-    for (var x = 50; x < width; x += space) 
+//    for (var x = 50; x < width; x += space) 
         
-    var column = 40;
-    var row = 20;
-    
-    //windows 
-    var windowx = 150;
-    var windowy = 50;
-    var windowh = 90;
-    var windowl = 60;
-    var window2y = 130;
 
     
+    //windows 
+//    var windowx = 150;
+//    var windowy = 50;
+//    var windowh = 90;
+//    var windowl = 60;
+//    var window2y = 130;
+
+    var column = width/4;
+    var row = height/3;
     
     //brick building
 	rectMode(CENTER);
-	for (var x = 0; x <= width; x += column) {
+	for (var x = 150; x <= width; x += column) {
 		line(x, 0, x, height);
-		for (var y = 0; y <= height; y += row) {
-			line(0, y, width, y);
+		for (var y = 50; y <= height; y += row) {
+            
+            // bricks
+			//line(0, y, width, y);
+            
+            //windows
+            stroke("black");
+            strokeWeight(2);
+            fill("powderblue");
+            rect(x,y,90,60);
+             noStroke();
+            strokeWeight(2);
+            fill("saddlebrown ");
+            rect(x,y + 30,110,10)
+
+            //face
+            stroke("black");
+            strokeWeight(1);
+            fill("yellow");
+            ellipse(x, y-4 , 44, 44);
+            
+            //left-eye
+            stroke("black");
+            strokeWeight(1);
+            fill("black");
+            ellipse(x - 10 ,y - 10,6,6);
+            
+            //right-eye
+            stroke("black");
+            strokeWeight(1);
+            fill("black");
+            ellipse(x + 10, y - 10,6,6);
+            
+            //mouth
+            fill("red");
+            arc(x, y, 20, 20, 0,PI, CHORD);
+            
+            
 		}
     }
 
@@ -37,29 +73,18 @@ function setup() {
 	rect(40,100,80,550);
     rect(600,100,80,550);
     
-    //windows
-    stroke("black");
-    strokeWeight(2);
-    fill("ghostwhite");
-    rect(windowx,windowy,windowh,windowl);
-        //windowx = windowx +110;
-    rect(windowx + 110,windowy,windowh,windowl);
-    rect(windowx + 220,windowy,windowh,windowl);
-    rect(windowx + 330,windowy,windowh,windowl);
     
-    rect(windowx,130,windowh,windowl);
-    rect(windowx + 110,window2y,windowh,windowl);
-    rect(windowx + 220,window2y,windowh,windowl);
-    rect(windowx + 330,window2y,windowh,windowl);
-
-
-    
-
-    
-    
-    
-
-
+//        windowx = windowx +110;
+//    rect(windowx + 110,windowy,windowh,windowl);
+//    rect(windowx + 220,windowy,windowh,windowl);
+//    rect(windowx + 330,windowy,windowh,windowl);
+//    
+//    rect(window6,130,windowh,windowl);
+//    rect(windowx + 110,window2y,windowh,windowl);
+//    rect(windowx + 220,window2y,windowh,windowl);
+//    rect(windowx + 330,window2y,windowh,windowl);
+   
+   
 
     
     
